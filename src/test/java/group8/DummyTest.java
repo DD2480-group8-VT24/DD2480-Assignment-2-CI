39925d05c52dee5c4d5e3e6b5aa568c342896bae
Atheer <atheerwaled66@hotmail.se>
2024-02-07 15:25:39 +0100

    test: add dummy test

---
+++ src/test/java/group8/DummyTest.java
@@ -0,0 +1,13 @@
+package group8;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+
+}
