ae0de2c99abd14bec6c5d5cbc80b888edb8898b7
Melissa Mazura <mazura@kth.se>
2024-02-13 12:29:18 +0100

    fix: remove @test for test case

---
+++ src/test/java/group8/DummyTest.java
@@ -0,0 +1,20 @@
+package group8;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+    @Test
+    public void testIsTrue() {
+        assertTrue(true);
+    }
+    public void testIsFalse() {
+        assertTrue(false);
+    }
+
+}
