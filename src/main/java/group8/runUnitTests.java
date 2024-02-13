e01be984052b3e8dce859dccd921aaa82d6f576c
Melissa Mazura <mazura@kth.se>
2024-02-13 13:08:46 +0100

    feat: create a function to check if tests pass

--- src/main/java/group8/runUnitTests.java
+++ src/main/java/group8/runUnitTests.java
@@ -1,8 +1,10 @@
 package group8;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 /**
  * This class will run specific test cases or classes by using the function runTests.
@@ -20,7 +22,6 @@ public class runUnitTests {
     public static boolean runTests(String testCase) {
         ProcessBuilder pb = new ProcessBuilder("mvn", testCase, "test");
         File resultsFile = new File("test_results.txt");
-
         try {
             pb.redirectErrorStream(true);
             pb.redirectOutput(resultsFile);
@@ -28,10 +29,7 @@ public class runUnitTests {
             Process process = pb.start();
             boolean is_exited = process.waitFor(5, TimeUnit.MINUTES);
             if (is_exited) {
-                int exitCode = process.exitValue();
-                if (exitCode == 0) {
-                    return true;
-                }
+                return findSuccessBuild(resultsFile);
             }
             else {
                 process.destroy();
@@ -44,5 +42,23 @@ public class runUnitTests {
         } 
         return false;
     }
-
+    public static boolean findSuccessBuild(File resultsFile) {
+        try {
+            Scanner sc = new Scanner(resultsFile);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.indexOf("BUILD SUCCESS") != -1) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+        }
+        sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+    }
+    return false;
+    }
 }
