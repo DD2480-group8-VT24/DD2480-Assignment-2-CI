cb36c1d012a380e1fe7e5a7421742b9d29542970
Melissa Mazura <mazura@kth.se>
2024-02-13 12:14:28 +0100

    test: create unit tests to check testing

--- src/test/java/group8/testTesting.java
+++ src/test/java/group8/testTesting.java
@@ -3,10 +3,20 @@ package group8;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 public class testTesting {
 
     @Test
     public void checkTestTrue() {
-        assertTrue(runUnitTests.runTests("-Dtest=DummyTest"));
+        assertTrue(runUnitTests.runTests("-Dtest=DummyTest#testIsTrue"));
+    }
+    @Test
+    public void checkTestFalse() {
+        assertFalse(runUnitTests.runTests("-Dtest=DummyTest#testIsFalse"));
+    }
+    @Test(expected = IOException.class)
+    public void checkTestDoesntExist() {
+        runUnitTests.runTests("-Dtest=DummyTest#NoTestExists");   
     }
 }
