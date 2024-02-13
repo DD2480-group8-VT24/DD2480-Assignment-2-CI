1fe872bf5ba7c17509a168c642cf8d74c2f7874b
Marcus Odin <marodi@kth.se>
2024-02-13 13:06:02 +0100

    feat: creating directory and cloning repo into it
    Commit to try on other machine

--- src/main/java/group8/ContinuousIntegrationServer.java
+++ src/main/java/group8/ContinuousIntegrationServer.java
@@ -5,10 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class ContinuousIntegrationServer extends AbstractHandler
 {
@@ -28,9 +31,31 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // for example
         // 1st clone your repository
         // 2nd compile the code
+        JsonRequest newRequest = JsonRequest.readJsonFromRequest(request);
+        cloneRepo(newRequest.getRepoName());
 
         response.getWriter().println("CI job done");
     }
+
+    public boolean cloneRepo(String repoName){
+        File tempDir = new File("./src/repo");
+        try{
+            Git git = Git.cloneRepository()
+                .setURI(repoName)
+                .setDirectory(tempDir)
+                .call();
+        }
+        catch(GitAPIException e){
+            System.err.println("Failed to clone repository");
+        }
+
+        return false;
+    }
+
+    public void checkout(String id){
+
+    }
+
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
