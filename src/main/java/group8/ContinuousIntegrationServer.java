e458476c332f091fe4227ac5717ccc93064adf2a
Melissa Mazura <mazura@kth.se>
2024-02-13 11:51:01 +0100

    fix: delete unused import

--- src/main/java/group8/ContinuousIntegrationServer.java
+++ src/main/java/group8/ContinuousIntegrationServer.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
-import java.util.concurrent.TimeUnit;
 import java.io.*;
 
 import org.eclipse.jetty.server.Server;
