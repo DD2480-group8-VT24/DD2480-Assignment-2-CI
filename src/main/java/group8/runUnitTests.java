ee4f206bbf835d8625679bf71bd057a836ca9852
Melissa Mazura <mazura@kth.se>
2024-02-13 12:34:56 +0100

    docs: add javadocs documentation to the class

--- src/main/java/group8/runUnitTests.java
+++ src/main/java/group8/runUnitTests.java
@@ -4,8 +4,19 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This class will run specific test cases or classes by using the function runTests.
+ * @author: Melissa Mazura
+*/
 
 public class runUnitTests {
+    /**
+    * This function will return a boolean whether the test passes or not. It throws an excpetion when it cannot find the test name.
+    * @param: testcase name
+    * @return: boolean if the test failed or succeeded
+    * @author: Melissa Mazura
+    */
+
     public static boolean runTests(String testCase) {
         ProcessBuilder pb = new ProcessBuilder("mvn", testCase, "test");
         File resultsFile = new File("test_results.txt");
