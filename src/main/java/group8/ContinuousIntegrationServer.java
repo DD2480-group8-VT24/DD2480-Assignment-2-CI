21d664809c3f1bfa40821cd31fb6f6aa69d64283
Jonatan Tuvstedt <57487033+Glassar@users.noreply.github.com>
2024-02-13 20:08:11 +0100

    feat: Server pulls commit #4 (#17)
    * feat: creating directory and cloning repo into it
    
    Commit to try on other machine
    
    * fix: use newer jgit dependency
    
    * feat: add clone repo function
    
    Co-authored-by: OdinGG <marcus.odin@live.se>
    
    * feat: add checkout branch function
    
    Co-authored-by: OdinGG <marcus.odin@live.se>
    
    * feat: add checkout commit function
    
    Co-authored-by: OdinGG <marcus.odin@live.se>
    
    * feat: update handle to clone repo and checkout the specified commit
    
    Co-authored-by: OdinGG <marcus.odin@live.se>
    
    * test: add tests for repo checkout functions
    
    * refactor: move testing code to test file
    
    * fix: add static tags to functions
    
    * docs: add java docs to checkout functions
    
    * chore: add temp repo folder to gitignore
    
    ---------
    
    Co-authored-by: Marcus Odin <marodi@kth.se>
    Co-authored-by: Atheer <atheerwaled66@hotmail.se>
    Co-authored-by: OdinGG <marcus.odin@live.se>

--- src/main/java/group8/ContinuousIntegrationServer.java
+++ src/main/java/group8/ContinuousIntegrationServer.java
@@ -5,10 +5,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 import java.io.*;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
 
 public class ContinuousIntegrationServer extends AbstractHandler
 {
@@ -24,13 +30,75 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         System.out.println(target);
 
+        File tempDir = new File("repo");
+        if (tempDir.exists()) {
+            FileUtils.deleteDirectory(tempDir);
+        }
+
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
+        JsonRequest jsonRequest = JsonRequest.readJsonFromRequest(request);
+        try {
+            Git git = cloneRepo(tempDir, jsonRequest.getRepoCloneUrl());
+            checkoutBranch(git, jsonRequest.getBranchName());
+            checkoutCommit(git, jsonRequest.getCommitId());
+
+            //String workingDirectory = System.getProperty("user.dir");
+        }catch(GitAPIException e) {
+            System.err.println("error: " + e);
+        }
 
         response.getWriter().println("CI job done");
     }
+
+    /**
+     * Clones the requested repo into the folder specified in tempDir and returns a 
+     * git object referencing it
+     * 
+     * @param tempDir
+     * @param repoName
+     * @return
+     * @throws GitAPIException
+     * @author Marcus Odin
+     * @author Jonatan Tuvstedt
+     */
+    public static Git cloneRepo(File tempDir, String repoName) throws GitAPIException{
+        return Git.cloneRepository()
+            .setURI(repoName)
+            .setDirectory(tempDir)
+            .call();
+    }
+
+    /**
+     * Switches to the requested repo
+     * 
+     * @param git
+     * @param branchName
+     * @throws GitAPIException
+     * @throws IOException
+     * @author Marcus Odin
+     * @author Jonatan Tuvstedt
+     */
+    public static void checkoutBranch(Git git, String branchName) throws GitAPIException, IOException{
+        git.checkout().setName("origin/" + branchName).call();
+    }
+
+    /**
+     * Checks out the requested git commit
+     * 
+     * @param git
+     * @param commitId
+     * @throws GitAPIException
+     * @throws IOException
+     * @author Marcus Odin
+     * @author Jonatan Tuvstedt
+     */
+    public static void checkoutCommit(Git git, String commitId) throws GitAPIException, IOException {
+        git.checkout().setName(commitId).call();
+    }
+
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
