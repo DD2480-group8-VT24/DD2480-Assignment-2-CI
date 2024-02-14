5950d0488374d4412c6e9a420028d135376fb26e
Marcus Odin <marodi@kth.se>
2024-02-14 13:18:19 +0100

    docs: added javadoc for package.

---
+++ src/main/java/group8/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This is a simple CI server for the Assignment #2: Continuous Integration
+ * which when receiving a webhook from GitHub checks out the commit, runs
+ * mvn compile and mvn test and then sends a status notification to the
+ * GitHub commit in question.
+ * @author Mert Demirsü
+ * @author Melissa Mazura
+ * @author Marcus Odin
+ * @author Atheer Salim
+ * @author Jonatan Tuvstedt
+ * @version 2024.02.14
+ */
+package group8;
